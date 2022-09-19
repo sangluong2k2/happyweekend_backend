aa4750c1318f8ec01185cf47f4e8316daac9ca6b
giangnqph15195 <giangqu13579@gmail.com>
2022-09-19 10:14:08 +0700

    feat:  Categories function - Getone - Update -Delete

--- src/router/catrgories.js
+++ src/router/catrgories.js
@@ -1,9 +1,12 @@
 import {Router} from 'express'
-import { creat, getall } from '../controller/categories'
+import { creat, getall, getone, remove, update } from '../controller/categories'
 
 const router = Router()
 
 router.get("/categories", getall)
+router.get("/category/:slug", getone)
+router.delete("/category/:id", remove)
+router.put("/category/:id/edit", update)
 router.post("/categories", creat)
 
 
