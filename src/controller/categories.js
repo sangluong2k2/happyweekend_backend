eee8fc54d3b705b783cb03c85428cf8c6604b1f0
giangnqph15195 <giangqu13579@gmail.com>
2022-09-19 10:01:06 +0700

    feat: Add Category - Controler

--- src/controller/categories.js
+++ src/controller/categories.js
@@ -1,7 +1,18 @@
 import Category from '../models/categories'
-
+import slugify from 'slugify'
 
 export const getall = async (req,res) =>{
     const list = await Category.find().exec()
     res.json(list)
+}
+
+export const creat = async (req, res) =>{
+    req.body.slug = slugify(req.body.name)
+    try {
+        const add = await Category(req.body).save()
+        res.json(add)
+    } catch (error) {
+        
+    } 
+   
 }
--- src/router/catrgories.js
+++ src/router/catrgories.js
@@ -1,8 +1,10 @@
 import {Router} from 'express'
-import { getall } from '../controller/categories'
+import { creat, getall } from '../controller/categories'
 
 const router = Router()
 
 router.get("/categories", getall)
+router.post("/categories", creat)
+
 
 module.exports = router
