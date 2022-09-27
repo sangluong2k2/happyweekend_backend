32e2e6d79f72c9c911d40a7922905b91ea0b5076
giangnqph15195 <giangqu13579@gmail.com>
2022-09-27 21:56:56 +0700

    Feat: Creat function - Get Single - Add Room - Edit Room - Delete Rooms

--- src/controller/rooms.js
+++ src/controller/rooms.js
@@ -1,5 +1,15 @@
 import Room from "../models/room"
 import slugify from "slugify"
+
+export const creat = async (req, res)=> {
+    req.body.slug = slugify(req.body.name)
+    try {
+        const add = await Room(req.body).save()
+        res.json(add)
+    } catch (error) {
+
+    }
+}
 export const getAll = async (req, res)=> {
     try {
         const room = await Room.find().exec()
@@ -8,3 +18,32 @@ export const getAll = async (req, res)=> {
         
     }
 }
+
+export const getOne = async (req, res)=> {
+    try {
+        const room = await Room.find({ slug: req.params.slug }).exec()
+        res.json(room)
+    } catch (error) {
+        
+    }
+}
+
+
+export const remove = async (req,res)=>{
+    try {
+         const deleteRomm = await Room.findOneAndDelete({_id: req.params.id}).exec()
+         res.json(deleteRomm)
+    } catch (error) {
+        
+    }
+ }
+ 
+ export const update = async (req,res)=>{
+    req.body.slug = slugify(req.body.name)
+     try {
+         const newRoom = await Room.findOneAndUpdate({_id:req.params.id}, req.body,{new:true}).exec()
+         res.json(newRoom)
+     } catch (error) {
+         
+     }
+ }
--- src/router/rooms.js
+++ src/router/rooms.js
@@ -1,9 +1,13 @@
 const {Router} = require('express')
-const { getAll } = require('../controller/rooms')
+const { getAll, creat, getOne, remove, update } = require('../controller/rooms')
 
 const router = Router()
 
 
 router.get("/rooms",getAll)
+router.get("/rooms/:slug",getOne)
+router.post("/rooms",creat)
+router.delete("/rooms/:id/delete", remove)
+router.put("/rooms/:id/edit", update)
 
 module.exports = router
