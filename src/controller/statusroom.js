32d285cfa30201651912484d2d0e02bde3186813
giangnqph15195 <giangqu13579@gmail.com>
2022-10-05 10:09:33 +0700

    update

--- src/controller/statusroom.js
+++
@@ -1,6 +0,0 @@
-import StatusRoom from '../models/statusroom'
-
-export const getall = async (req,res) =>{
-    const list = StatusRoom.find().exec()
-    res.json(list)
-}
--- src/router/statusroom.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express')
-const { getall } = require('../controller/statusroom')
-
-const router = Router()
-
-router.get("/statusroom", getall)
-
-
-module.exports = router
