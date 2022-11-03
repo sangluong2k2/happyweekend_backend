af8a261c9325127fb8c22f2758286578a20fe5a3
sangluong2k2 <luongduysang2k2@gmail.com>
2022-11-03 09:34:07 +0700

    npm lastest mongodb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { readdirSync } from "fs";
+const { MongoClient, ServerApiVersion } = require('mongodb');
 import CategoryRouter from "./router/catrgories";
 import RoomRouter from "./router/rooms";
 
@@ -30,13 +31,22 @@ readdirSync("./src/router").forEach((route) => {
   app.use("/api", require(`./router/${route}`));
 });
 
-mongoose
-  .connect("mongodb://localhost:27017/happyweekend")
-  .then(() => console.log("connect db thanh cong"))
-  .catch((error) => console.log(error));
+// mongoose
+//   .connect("mongodb://localhost:27017/happyweekend")
+//   .then(() => console.log("connect db thanh cong"))
+//   .catch((error) => console.log(error));
 
-const PORT = 4000;
+// const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server running port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server running port ${PORT}`);
+// });
+
+
+const uri = "mongodb+srv://admin:<Sang2002!>@happyweekend.e5gnq2j.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
