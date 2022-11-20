b9239d6d38c4ea40498dde017a2a62862e8961ec
= <=>
2022-11-20 21:24:47 +0700

    edit search room

--- src/controller/rooms.js
+++ src/controller/rooms.js
@@ -2,6 +2,7 @@ import Room from "../models/room"
 import slugify from "slugify"
 import imagesroom from "../models/imagesroom"
 import dateBooked from "../models/dateBooked"
+import _ from "lodash"
 
 export const creat = async (req, res) => {
     req.body.slug = slugify(req.body.name)
@@ -55,11 +56,32 @@ export const search = async (req, res) => {
     const checkIn = req.query.dateFrom;
     const checkOut = req.query.dateTo;
     try {
-        const rooms = await dateBooked.find({
-            dateFrom: { $eq: checkIn },
-            dateTo: { $eq: checkOut }
+        const rooms = await Room.find().exec();
+        const bookedRooms = await dateBooked.find({
+            dateFrom: { $gte: checkIn },
+            dateTo: { $lte: checkOut }
         }).exec()
-        res.json(rooms)
+        if (!(checkOut === 'null')) {
+            let bookedRoomId = bookedRooms.map((item) => JSON.stringify(item.room));
+            const enqBookRoomId = _.uniq(bookedRoomId);
+            rooms.forEach((item, index) => {
+                enqBookRoomId.map((idItem) => {
+                    if (JSON.stringify(item._id) == idItem) {
+                        rooms.splice(index, 1);
+                    }
+                })
+                // var evens = _.remove([...rooms], function (itemId) {
+                //     return item == itemId;
+                // });
+                // console.log(evens)
+                if (index === rooms.length - 1) {
+                    res.json(rooms)
+                }
+            })
+        }
+        else {
+            res.json(rooms)
+        }
     } catch (error) {
 
     }
